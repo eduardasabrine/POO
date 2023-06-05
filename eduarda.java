d1485e29626891a61268fe6f873530fb9600313e
eduardasabrine <135068832+eduardasabrine@users.noreply.github.com>
2023-06-05 18:20:21 -0300

    Add files via upload

---
+++ eduarda.java
@@ -0,0 +1,27 @@
+Pessoa p1 = new Pessoa ();
+p1.nome = "Maria";
+p1.idade = 18;
+p1.sexo = 'F'
+p1.cpf = "123.456.789-00"
+
+Pessoa p2 = new Pessoa();
+p2.nome = "Joao";
+p2idade. = 22;
+p2.sexo = 'M'
+p2.cpf = "234.567.890-00"
+
+Conta c1 = new Conta();
+c1.numero =  "1234-5"
+c1.titular = p1; 
+c1.saldo = 100.0;
+c1.limite = 200.0;
+
+Conta c2 = new Conta();
+c1.numero =  "2345-6"
+c1.titular = p2; 
+c1.saldo = 150.0;
+c1.limite = 200.0;
+
+csyste=.out.printlnl (c1.saldo);
+csyste=.out.println1 (c1.titular.nome);
+csyste=.out.println1 (c1.Saldo da conta " + c2.numero+" + c2.saldo);
